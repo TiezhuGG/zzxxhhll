1295de048b01612b1ca181982b536a5fa75a5dca
XIAOXIN <2530805269@qq.com>
2020-04-29 15:03:43 +0800

    合并

--- src/router/index.js
+++ src/router/index.js
@@ -41,68 +41,68 @@ export const constantRoutes = [
     component: resolve => require(['@/views/user/index'], resolve),
     hidden: true,
     children: [{
-        path: 'login',
-        name: 'Login',
-        component: resolve => require(['@/views/user/login/login'], resolve),
-        hidden: true,
-        meta: {
-          title: '登录'
-        }
-      },
-      {
-        path: 'register',
-        name: 'Register',
-        component: resolve => require(['@/views/user/register/register'], resolve),
-        hidden: true,
-        meta: {
-          title: '注册'
-        }
-      },
-      {
-        path: 'verify-code',
-        name: 'VerifyCode',
-        component: resolve => require(['@/views/user/verify-code/verify-code'], resolve),
-        hidden: true,
-        meta: {
-          title: '验证码'
-        }
-      },
-      {
-        path: 'set-password',
-        name: 'SetPassword',
-        component: resolve => require(['@/views/user/set-password/set-password'], resolve),
-        hidden: true,
-        meta: {
-          title: '设置密码'
-        }
-      },
-      {
-        path: 'enterprise',
-        name: 'Enterprise',
-        component: resolve => require(['@/views/user/enterprise/enterprise'], resolve),
-        hidden: true,
-        meta: {
-          title: '注册企业'
-        }
-      },
-      {
-        path: 'has-enterprise',
-        name: 'HasEnterprise',
-        component: resolve => require(['@/views/user/has-enterprise/has-enterprise'], resolve),
-        hidden: true,
-        meta: {
-          title: '已有企业'
-        }
-      },
-      {
-        path: 'unreviewed',
-        name: 'Unreviewed',
-        component: resolve => require(['@/views/user/unreviewed/unreviewed'], resolve),
-        hidden: true,
-        meta: {
-          title: '申请企业未审批'
-        }
-      },
+      path: 'login',
+      name: 'Login',
+      component: resolve => require(['@/views/user/login/login'], resolve),
+      hidden: true,
+      meta: {
+        title: '登录'
+      }
+    },
+    {
+      path: 'register',
+      name: 'Register',
+      component: resolve => require(['@/views/user/register/register'], resolve),
+      hidden: true,
+      meta: {
+        title: '注册'
+      }
+    },
+    {
+      path: 'verify-code',
+      name: 'VerifyCode',
+      component: resolve => require(['@/views/user/verify-code/verify-code'], resolve),
+      hidden: true,
+      meta: {
+        title: '验证码'
+      }
+    },
+    {
+      path: 'set-password',
+      name: 'SetPassword',
+      component: resolve => require(['@/views/user/set-password/set-password'], resolve),
+      hidden: true,
+      meta: {
+        title: '设置密码'
+      }
+    },
+    {
+      path: 'enterprise',
+      name: 'Enterprise',
+      component: resolve => require(['@/views/user/enterprise/enterprise'], resolve),
+      hidden: true,
+      meta: {
+        title: '注册企业'
+      }
+    },
+    {
+      path: 'has-enterprise',
+      name: 'HasEnterprise',
+      component: resolve => require(['@/views/user/has-enterprise/has-enterprise'], resolve),
+      hidden: true,
+      meta: {
+        title: '已有企业'
+      }
+    },
+    {
+      path: 'unreviewed',
+      name: 'Unreviewed',
+      component: resolve => require(['@/views/user/unreviewed/unreviewed'], resolve),
+      hidden: true,
+      meta: {
+        title: '申请企业未审批'
+      }
+    }
     ]
   },
 
@@ -116,24 +116,23 @@ export const constantRoutes = [
     component: resolve => require(['@/views/invite/index'], resolve),
     hidden: true,
     children: [{
-        path: 'member',
-        name: 'Member',
-        component: resolve => require(['@/views/invite/member/member'], resolve),
-        hidden: true,
-        meta: {
-          title: '邀请加入团队'
-        }
-      },
-      {
-        path: 'write-name',
-        name: 'WriteName',
-        component: resolve => require(['@/views/invite/write-name/write-name'], resolve),
-        hidden: true,
-        meta: {
-          title: '填写姓名'
-        }
-      },
-    ]
+      path: 'member',
+      name: 'Member',
+      component: resolve => require(['@/views/invite/member/member'], resolve),
+      hidden: true,
+      meta: {
+        title: '邀请加入团队'
+      }
+    },
+    {
+      path: 'write-name',
+      name: 'WriteName',
+      component: resolve => require(['@/views/invite/write-name/write-name'], resolve),
+      hidden: true,
+      meta: {
+        title: '填写姓名'
+      }
+    }]
   },
   {
     path: '/application',
@@ -187,45 +186,43 @@ export const constantRoutes = [
     redirect: '/apps/payroll',
     hidden: true,
     children: [{
-        path: 'payroll',
-        name: 'Payroll',
-        component: resolve => require(['@/views/apps/payroll/index'], resolve),
-        meta: {
-          title: '公司工资薪资表',
-          icon: 'payroll',
-          activeMenu: '/index'
-        }
-      }, {
-        path: 'detail',
-        name: 'Detail',
-        component: resolve => require(['@/views/apps/payroll/detail'], resolve),
-        meta: {
-          title: '个人信息',
-          icon: 'payroll',
-          activeMenu: '/index'
-        }
-      },
-      {
-        path: '/order',
-        name: 'Order',
-        component: resolve => require(['@/views/apps/order/order'], resolve),
-        meta: {
-          title: '客户订单汇总管理',
-          icon: "collect",
-          activeMenu: '/index'
-        },
-      },
-      {
-        path: '/order/order-detail',
-        name: 'Order-detail',
-        component: resolve => require(['@/views/apps/order/order-detail'], resolve),
-        meta: {
-          title: '客户订单汇总管理',
-          icon: "collect",
-          activeMenu: '/index'
-        },
-      },
-    ]
+      path: 'payroll',
+      name: 'Payroll',
+      component: resolve => require(['@/views/apps/payroll/index'], resolve),
+      meta: {
+        title: '公司工资薪资表',
+        icon: 'payroll',
+        activeMenu: '/index'
+      }
+    }, {
+      path: 'detail',
+      name: 'Detail',
+      component: resolve => require(['@/views/apps/payroll/detail'], resolve),
+      meta: {
+        title: '个人信息',
+        icon: 'payroll',
+        activeMenu: '/index'
+      }
+    },
+    {
+      path: '/order',
+      name: 'Order',
+      component: resolve => require(['@/views/apps/order/order'], resolve),
+      meta: {
+        title: '客户订单汇总管理',
+        icon: 'collect',
+        activeMenu: '/index'
+      }
+    }, {
+      path: '/order/order-detail',
+      name: 'Order-detail',
+      component: resolve => require(['@/views/apps/order/order-detail'], resolve),
+      meta: {
+        title: '客户订单汇总管理',
+        icon: 'collect',
+        activeMenu: '/index'
+      }
+    }]
   },
 
   {
